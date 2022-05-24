5d6b36a7f51cc87ef4396474758045005dda563b
Kali <mm.mikou.marouane@gmail.com>
2022-05-24 08:09:54 -0400

    first commit

---
+++ tst1.py
@@ -0,0 +1,19 @@
+
+
+def func():
+    try:
+        for i in range(5):
+            print("HI ", i)
+        x = str(input("enter your name"))
+        return x
+    except Exception as e:
+        raise e
+
+
+def func():
+    try:
+        print("hello")
+        func()
+        x=2
+    except Exception as e:
+        return "Exception ", e
